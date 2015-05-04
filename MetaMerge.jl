bf5a0286185c4f7076e6c7795f49be6985f78f26
davidagold <david.gold.a@gmail.com>
2015-05-04 13:49:21 -0400

    naming & tweaks
    Changed ‘metamerge()’ to ‘merge!()’; improved robustness so that
    merge!() works in both general code and in REPL.

--- MetaMerge.jl
+++ MetaMerge.jl
@@ -2,24 +2,18 @@ module MetaMerge
 
 importall Main
 
-export metamerge
+export merge!
 
-function makemethod(f::Function, argtypes::Tuple, modulename::Symbol)
+function makemethod(expr_fmerged, expr_fmodule::Expr, argtypes::Tuple)
 
-    # Note that passing a value such as 'A.f' to makemethod() will drop the module
-    # from the function name. We must therefore add it to the expression passed to
-    # RHS (below):
-    expr_Modulef = Expr(:., Expr(:., symbol("Main"), QuoteNode(modulename)), QuoteNode(symbol("$f")))
-    expr_Mainf = Expr (:., symbol("Main"), QuoteNode(symbol("$f")))
+    # Expression calling fmerged
+    LHS = Expr(:call, expr_fmerged)
 
-    # Expression calling 'Main.f'
-    LHS = Expr(:call, expr_Mainf)
+    # Expression calling function denoted by expr_fmodule
+    RHS = Expr(:call, expr_fmodule)
 
-    # Expression calling 'Main.Module.f'
-    RHS = Expr(:call, expr_Modulef)
-
-    # Adds argument symbols with type annotations to signature of call to 'Main.f'
-    # Adds just argument symbols to call of 'Main.Module.f'
+    # Adds argument symbols with type annotations to signature of call to 'fmerged'
+    # Adds just argument symbols to call of 'fmodule'
     for (i, arg) in enumerate(argtypes)
         push!(LHS.args, Expr(:(::), symbol("x$i"), arg))
         push!(RHS.args, symbol("x$i"))
@@ -27,95 +21,77 @@ function makemethod(f::Function, argtypes::Tuple, modulename::Symbol)
     # println(LHS)
     # println(RHS)
 
-    # Sets the calls of 'newf' and 'oldf' equal to one another and evaluates.
-    exeq = Expr(:(=), LHS, RHS)
-    eval(exeq)
+    # Set the calls of 'newf' and 'oldf' equal to one another and evaluates.
+    ex_eq = Expr(:(=), LHS, RHS)
+
+    # Generate method
+    # println(ex_eq)
+    eval(ex_eq)
 end
 
-function metamerge(f::Function, module_A::Module, module_B::Module; conflicts_favor=Module)
+function merge!(fmerged::Function, modfuncA::(Module, Function), modfuncB::(Module, Function); conflicts_favor=Module)
 
-    # Generate symbols for $module_A.$f, $module_B.$f
-    expr_fA = Expr(:., Expr(:., symbol("Main"), QuoteNode(symbol("$module_A"))), QuoteNode(symbol("$f")))
-    expr_fB = Expr(:., Expr(:., symbol("Main"), QuoteNode(symbol("$module_B"))), QuoteNode(symbol("$f")))
+    # Generate symbol for module calling mergemethods()
+    # Hereafter the calling module will be known as 'here'.
+    symb_here = symbol("$(current_module())")
 
-    # Generate calls to functions $module_A.$f, $module_B.$f
-    expr_fAcall = Expr(:call, expr_fA)
-    expr_fBcall = Expr(:call, expr_fB)
+    # Generate expression for Main.here.fmerged
+    expr_here = Expr(:., symbol("Main"), QuoteNode(symb_here))
+    expr_fmerged = Expr(:., expr_here, QuoteNode(symbol("$fmerged")))
 
-    # Generate arrays of Method objects for f, g
-    const fA_methods = methods(@eval($expr_fA), (Any...))
-    const fB_methods = methods(@eval($expr_fB), (Any...))
+    # Generate symbols for fA, fB
+    symb_moduleA = symbol("$(modfuncA[1])")
+    symb_moduleB = symbol("$(modfuncB[1])")
 
-    # Generate arrays of method signatures for A.f, B.f.
+    # Generate symbols for moduleA, moduleB
+    fA = symbol("$(modfuncA[2])")
+    fB = symbol("$(modfuncB[2])")
+
+    # Generate expressions for Main.moduleA, Main.moduleB
+    expr_MainA = Expr(:., symbol("Main"), QuoteNode(symb_moduleA))
+    expr_MainB = Expr(:., symbol("Main"), QuoteNode(symb_moduleB))
+
+    # Generate expressions for Main.ModuleA.fA, Main.ModuleB.fB
+    expr_fA = Expr(:., expr_MainA, QuoteNode(fA))
+    expr_fB = Expr(:., expr_MainB, QuoteNode(fB))
+
+    # Generate arrays of Method objects for Main.ModuleA.fA, Main.ModuleB.fB
+    const methods_fA = methods(eval(expr_fA), (Any...))
+    const methods_fB = methods(eval(expr_fB), (Any...))
+
+    # Generate arrays of method signatures for Main.ModuleA.fA, Main.ModuleB.fB
     # Note that the 'sig' field of a Method is (as of 3.7) a tuple of types.
-    fA_sigs = [ fA_methods[i].sig for i in 1:length(fA_methods) ]
-    fB_sigs = [ fB_methods[i].sig for i in 1:length(fB_methods) ]
+    const sigs_fA = [ methods_fA[i].sig for i in 1:length(methods_fA) ]
+    const sigs_fB = [ methods_fB[i].sig for i in 1:length(methods_fB) ]
 
     # Generate array of overlapping signatures of A.f, B.f, i.e. ambiguous cases.
-    const AB_intr = intersect(fA_sigs, fB_sigs)
+    const conflicts = intersect(sigs_fA, sigs_fB)
 
-    # Adjust either fA_sigs or fB_sigs according to whether caller wishes cases
+    # Adjust either sigs_fA or sigs_fB according to whether caller wishes cases
     # of ambiguous signatures to be handled by A.f or B.f. If 'conflicts_favor'
     # is void or equal to neither A nor B, then no method is assigned for the
     # ambiguous signature.
-    if conflicts_favor == module_A
-        fB_sigs = setdiff(fB_sigs, AB_intr)
-    elseif conflicts_favor == module_B
-        fA_sigs = setdiff(fA_sigs, AB_intr)
+    if conflicts_favor == eval(expr_MainA)
+        sigs_fB = setdiff(sigs_fB, conflicts)
+    elseif conflicts_favor == eval(expr_MainB)
+        sigs_fA = setdiff(sigs_fA, conflicts)
     else
-        fB_sigs = setdiff(fB_sigs, AB_intr)
-        fA_sigs = setdiff(fA_sigs, AB_intr)
+        sigs_fB = setdiff(sigs_fB, conflicts)
+        sigs_fA = setdiff(sigs_fA, conflicts)
     end
 
-    # Loop through elements of fA_sigs and assign appropriate methods of A.f to f
-    for (i, sig) in enumerate(fA_sigs)
+    # Loop through elements of sigs_fA and assign appropriate methods of A.f to h
+    for (i, sig) in enumerate(sigs_fA)
         # println(sig)
-        makemethod(f, sig, symbol("$module_A"))
+        makemethod(expr_fmerged, expr_fA, sig)
     end
 
-    # Loop through elements of fB_sigs and assign appropriate methods of B.f to f
-    for (i, sig) in enumerate(fB_sigs)
+    # Loop through elements of sigs_fB and assign appropriate methods of B.f to h
+    for (i, sig) in enumerate(sigs_fB)
         # println(sig)
-        makemethod(f, sig, symbol("$module_B"))
+        makemethod(expr_fmerged, expr_fB, sig)
     end
 
-    return f
+end # Function merge!(fmerged::Function, modfuncA::(Module, Function), modfuncB::(Module, Function); conflicts_favor=Module)
 
-end # Function metamerge(f::Function, module_A::Module, module_B::Module; conflicts_favor=Module)
-
-function metamerge(module_A::Module, module_B::Module; conflicts_favor=Module)
-
-    const names_A = names(module_A)
-    const names_B = names(module_B)
-
-    # Initialize arrays of functions with names exported by A, B
-    const functions_A = Any[]
-    const functions_B = Any[]
-
-    # Add to functions_A functions exported by A
-    for (i, name) in enumerate(names_A)
-        A_name = Expr(:(.), symbol("$module_A"), QuoteNode(symbol("$name")))
-        println(A_name)
-        isa(eval(A_name), Function) == true && push!(functions_A, eval(name))
-    end
-
-    # Add to functions_B functions exported by B
-    for (i, name) in enumerate(names_B)
-        B_name = Expr(:(.), symbol("$module_B"), QuoteNode(symbol("$name")))
-        isa(eval(B_name), Function) == true && push!(functions_B, eval(name))
-    end
-
-    # Generate an array of functions exported and identically named by both A and B
-    const functions_intr = intersect(functions_A, functions_B)
-
-    # Merge functions in functions_intr
-    for (i, f) in enumerate(functions_intr)
-        metamerge(f, module_A, module_B, conflicts_favor=@eval($conflicts_favor))
-        # println("Merged function $f with methods from $module_A, $module_B.")
-    end
-
-    return functions_intr
-
-end # Function metamerge(module_A::Module, module_B::Module; conflicts_favor=Module)
-
-end # Module MetaMerge
+end # Module MergeMethods
